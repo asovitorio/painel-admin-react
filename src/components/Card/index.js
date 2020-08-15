e0ab0ea300f042666f183928aa44edcd85be531f
Alessandro <asovitorio@gmail.com>
2020-08-15 02:44:34 -0300

    feature  card-groups concluido

---
+++ src/components/Card/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+
+export default function Card (props) {
+     
+    return(
+    <div className="card" id={props.id} >
+        <div className="card-date">
+                <h4 >{props.name}</h4>
+                <h1>{props.number}</h1>
+        </div>
+        <div className="card-icon">
+           <i>{props.icon}</i> 
+        </div>           
+    </div>
+    )
+}
--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './style.css'
+import {FaClipboardList,FaDollarSign,FaUserCheck} from 'react-icons/fa'
+import Card from '../../components/Card'
 
 export default  function Section(){
+
     return (
         <section>
-        <h1>App Dashboard</h1>
+        <h1 className="title">App Dashboard</h1>
+        <article className="card-group">
+        <Card id="blue" name="PRODUCTS IN DATA BASE" number="135"  icon={<FaClipboardList size={50} color="#d3d3d3" />} />
+        <Card id="green"  name="AMOUNT IN PRODUCTS" number="$546.456"  icon={<FaDollarSign size={50} color="#d3d3d3" />} />
+        <Card id="yellow" name="USER QUANTITY" number="38"  icon={<FaUserCheck size={50} color="#d3d3d3" />} />
+        </article>
         </section>
     )
 }
